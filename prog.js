74b1b5adfd960b6d7d9f64291eaef9297f5e5077
Rhonda Eisenhauer <rhonda.s.eisenhauer@live.com>
2015-07-22 10:36:02 -0500

    Improved code

--- prog.js
+++ prog.js
@@ -2,23 +2,30 @@ function getQuo(input) {
 	return input / 10;
 }
 
-getQuo(10);
 
 function getProd(input) {
 	return input * 5;
 }
 
-getProd(5);
 
-var num = document.getElementById("value").addEventListener("click", checkValue);
-var checkValue = ""
+
+
+
 
 function validateForm() {
-  if (num.value === "") 
+  var num = document.getElementById("value").value;
+  if (num === "") {
    alert("Must enter a value");
+} else if (num > 10000) {
+	output.innerHTML = getQuo(num);
+} else if (num < 10000) {
+	output.innerHTML = getProd(num);
+}
         
-    }
+}
     console.log(num.value)
+
+
 
 
 

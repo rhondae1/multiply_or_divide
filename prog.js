be2d315c689759e2141e6b037eddbb71e43e35f3
Rhonda Eisenhauer <rhonda.s.eisenhauer@live.com>
2015-07-22 10:07:53 -0500

    left off on alertfunction

--- prog.js
+++ prog.js
@@ -1,10 +1,24 @@
-function quo(imput, 10) {
-	return = input / 10;
+function getQuo(input) {
+	return input / 10;
 }
 
-function prod(imput, 5) {
-	return = input * 5;
+getQuo(10);
+
+function getProd(input) {
+	return input * 5;
 }
+
+getProd(5);
+
+var num = document.getElementById("value").addEventListener("click", checkValue);
+var checkValue = ""
+
+function validateForm() {
+  if (num.value === "") 
+   alert("Must enter a value");
+        
+    }
+    console.log(num.value)
 
 
 
